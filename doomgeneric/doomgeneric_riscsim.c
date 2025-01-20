b11cce2a0cbf69b453305737c0340a5c090608d3
Dawid Pietrykowski <d.pietrykowski16@gmail.com>
2025-01-20 21:34:52 +0100

    Read key queue

--- doomgeneric/doomgeneric_riscsim.c
+++ doomgeneric/doomgeneric_riscsim.c
@@ -15,55 +15,34 @@
 
 #define KEYQUEUE_SIZE 16
 
-static unsigned short s_KeyQueue[KEYQUEUE_SIZE];
-static unsigned int s_KeyQueueWriteIndex = 0;
+static uint32_t s_KeyQueue[KEYQUEUE_SIZE];
 static unsigned int s_KeyQueueReadIndex = 0;
 
-static unsigned char convertToDoomKey(unsigned int key)
-{
-  // return key;
-  return 0;
-}
-
-static void addKeyToQueue(int pressed, unsigned int keyCode)
-{
-  unsigned char key = convertToDoomKey(keyCode);
-
-  unsigned short keyData = (pressed << 8) | key;
-
-  s_KeyQueue[s_KeyQueueWriteIndex] = keyData;
-  s_KeyQueueWriteIndex++;
-  s_KeyQueueWriteIndex %= KEYQUEUE_SIZE;
-}
-
 #define SCREEN_ADDR (0x1000000 - 4)
+#define KEY_QUEUE_ADDR (0x1000000 - 8)
 uint32_t* const screen_addr = (uint32_t*)SCREEN_ADDR;
+uint32_t* const key_queue_addr = (uint32_t*)KEY_QUEUE_ADDR;
+uint32_t ms_sim = 0;
 struct timeval stop, start;
 
 void DG_Init()
 {
   gettimeofday(&start, NULL);
-  printf("dg init\n");
 }
 
 
 void DG_DrawFrame()
 {
   *screen_addr = (uint32_t)DG_ScreenBuffer;
-  // printf("Set addr: %x\n", *(uint32_t*)SCREEN_ADDR);
-  // uint32_t size = DOOMGENERIC_RESX*DOOMGENERIC_RESY*sizeof(uint32_t);
-  // memcpy(SCREEN_ADDR, DG_ScreenBuffer, size);
-  // memset(SCREEN_ADDR + size, 1, 1);
 }
 
-uint32_t ms_sim = 0;
 
 void DG_SleepMs(uint32_t ms)
 {
-  // memset(0x12345678, 0, 100); // temp delay
   uint32_t ticks = DG_GetTicksMs();
   while (DG_GetTicksMs() - ticks < ms) {};
 }
+
 uint32_t DG_GetTicksMs()
 {
     gettimeofday(&stop, NULL);
@@ -82,36 +61,31 @@ uint32_t DG_GetTicksMs()
 
 int DG_GetKey(int* pressed, unsigned char* doomKey)
 {
-  // printf("get key\n");
-  // handleKeyInput();
-  if (s_KeyQueueReadIndex == s_KeyQueueWriteIndex)
-  {
-    //key queue is empty
-    return 0;
-  }
-  else
-  {
-    unsigned short keyData = s_KeyQueue[s_KeyQueueReadIndex];
+  uint32_t keyData = s_KeyQueue[s_KeyQueueReadIndex];
+  if (keyData != 0xFFFFFFFF) {
     s_KeyQueueReadIndex++;
     s_KeyQueueReadIndex %= KEYQUEUE_SIZE;
 
-    *pressed = keyData >> 8;
+    *pressed = keyData >> 31;
     *doomKey = keyData & 0xFF;
 
     return 1;
   }
 
+  s_KeyQueue[0] = 0xFFFFFFFF;
+  s_KeyQueueReadIndex = 0;
   return 0;
 }
 
 void DG_SetWindowTitle(const char * title)
 {
-  printf("DG_SetWindowTitle: %s\n", title);
+
 }
 
 int main(int argc, char **argv)
 {
     doomgeneric_Create(argc, argv);
+    *key_queue_addr = (uint32_t)s_KeyQueue;
 
     for (int i = 0; ; i++)
     {
